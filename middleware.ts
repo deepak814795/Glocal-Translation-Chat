13e832305b53b4d99f5fe2426ad590839b88652f
Deepak Kushwaha <deepak403419@gmail.com>
2024-03-08 00:43:47 +0530

    private paths [/chat, /chat/chatId, /register]

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth;
+export default withAuth;  
 
 export const config = {
     matcher: ["/chat", "/chat/:id*", "/register"],
