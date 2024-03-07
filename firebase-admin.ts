8c03328fa701bf3e5d38b532d450c284b4d2e6a8
Deepak Kushwaha <deepak403419@gmail.com>
2024-03-08 00:41:58 +0530

    get private key/projectId

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -8,7 +8,7 @@ if(!admin.apps.length){
         credential: admin.credential.cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY, 
         }),
     });
 }
