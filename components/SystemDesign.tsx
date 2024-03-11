625121c96d1641332ce244bf602dd6082874018c
Deepak Kushwaha <deepak403419@gmail.com>
2024-03-12 01:08:53 +0530

    done

--- components/SystemDesign.tsx
+++ components/SystemDesign.tsx
@@ -149,7 +149,7 @@ const SystemDesign: React.FC = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  <span className="mx-2">Backend API Server</span>
+                  <span className="mx-2">Backend Server</span>
                 </p>
                 <p className="flex items-center -mx-2 text-gray-700 dark:text-gray-200">
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-2 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
