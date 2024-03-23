dd4a529bd6d586a1315b639084214d655c09c97a
Deepak Kushwaha <lucifer89240@gmail.com>
2024-03-24 01:48:02 +0530

    firebaseconfig

--- firebase.ts
+++ firebase.ts
@@ -6,11 +6,11 @@ import { getFunctions }  from "firebase/functions"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCJ7UZRjI-3q7vESt9UaPDF08DpYq-XKVo",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID_FOR_FIREBASETS,
-  storageBucket: "saas-chat-app-dc1dc.appspot.com",
-  messagingSenderId: "785038743864",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app=getApps().length ? getApp() : initializeApp(firebaseConfig);
