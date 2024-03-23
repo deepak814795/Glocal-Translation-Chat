f75331c0193ea1c8d8374a94f3456e37ee5f0a9b
Deepak Kushwaha <lucifer89240@gmail.com>
2024-03-23 21:47:13 +0530

    updated firebaseconfig

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,7 @@
 import{getApp, getApps, initializeApp} from "firebase/app"
 import {getAuth} from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-import { getFunctions } from "firebase/functions"
+import { getFunctions }  from "firebase/functions"
 
 
 const firebaseConfig = {
